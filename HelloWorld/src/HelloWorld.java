50545d9af26bafc5d47bb71a47d00e8782e70c58
martev <martin.evtimov.86@gmail.com>
2019-09-29 19:00:50 +0300

    first java code

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,14 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		System.out.println("HelloWorld:)");
+		System.out.println("HelloWorld :(");
+
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
